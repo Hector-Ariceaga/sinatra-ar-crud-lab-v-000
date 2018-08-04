ab1d9545ecdaaf9c4293ecd50d7022196258db9c
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-08-04 22:50:55 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,18 +14,25 @@ class ApplicationController < Sinatra::Base
   
   post '/posts' do
     @post = Post.create(params)
+<<<<<<< HEAD
     
+=======
+>>>>>>> 978cdd88540c0ce53d3c02494233bcf2ef4c15ca
     redirect_to '/posts'
   end
   
   get '/posts' do
     @posts = Post.all
+<<<<<<< HEAD
     
+=======
+>>>>>>> 978cdd88540c0ce53d3c02494233bcf2ef4c15ca
     erb :"/posts/index"
   end
   
   get '/posts/:id' do 
     @post = Post.find(params[:id])
+<<<<<<< HEAD
     
     erb :"/posts/show"
   end
@@ -46,6 +53,25 @@ class ApplicationController < Sinatra::Base
     @post = Post.find(params[:id])
     Post.delete(params[:id])
     
+=======
+    erb :"/posts/show"
+  end
+  
+  patch '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    Post.update(params[:id], name: params[:name], content: params[:content])
+    redirect_to '/posts/:id'
+  end
+  
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :"/posts/edit"
+  end 
+  
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    Post.delete(params[:id])
+>>>>>>> 978cdd88540c0ce53d3c02494233bcf2ef4c15ca
     erb :"/posts/delete"
   end
   
